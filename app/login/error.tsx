8212a147a1afb3b616efa92a4094c4f01243365f
Albert Céspedes Esteve <albertcespedes@gmail.com>
2023-12-02 12:22:59 +0100

    punt inicial

---
+++ app/login/error.tsx
@@ -0,0 +1,97 @@
+"use client";
+import { lusitana } from "@/app/ui/fonts";
+import AcmeLogo from "@/app/ui/acme-logo";
+import { Button } from "../ui/button";
+
+import {
+  AtSymbolIcon,
+  KeyIcon,
+  ExclamationCircleIcon,
+} from "@heroicons/react/24/outline";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+            <AcmeLogo />
+          </div>
+        </div>
+        <form className="space-y-3">
+          <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+            <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+              Please log in to continue.
+            </h1>
+            <div className="w-full">
+              <div>
+                <label
+                  className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <div className="relative">
+                  <input
+                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder="Enter your email address"
+                    required
+                  />
+                  <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                </div>
+              </div>
+              <div className="mt-4">
+                <label
+                  className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <div className="relative">
+                  <input
+                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                    id="password"
+                    type="password"
+                    name="password"
+                    placeholder="Enter password"
+                    required
+                    minLength={6}
+                  />
+                  <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                </div>
+              </div>
+            </div>
+            <Button
+              onClick={
+                // Attempt to recover by trying to re-render the invoices route
+                () => reset()
+              }
+            >
+              Back
+            </Button>
+            {/* Error */}
+            <div
+              className="flex h-8 items-end space-x-1"
+              aria-live="polite"
+              aria-atomic="true"
+            >
+              <>
+                <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+                <p className="text-sm text-red-500">Invalid credentials</p>
+              </>
+            </div>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+}
