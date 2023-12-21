d3cb76c4d5890110c879017b0c6f008e2bb21712
Albert Céspedes Esteve <albertcespedes@gmail.com>
2023-12-21 11:05:00 +0100

    overview+customers

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -16,25 +16,28 @@ export default async function Page() {
   if (user) {
     // Use the `user` variable here
     const reve = await fetchRevenuePerMonth(user);
+
+    return (
+      <main>
+        <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+          Dashboard <span className="text-sm ml-4">{user.name}</span>
+        </h1>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+          <Suspense fallback={<CardsSkeleton />}>
+            <CardWrapper />
+          </Suspense>
+        </div>
+        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+          <Suspense fallback={<RevenueChartSkeleton />}>
+            <RevenueChart />
+          </Suspense>
+          <Suspense fallback={<LatestInvoicesSkeleton />}>
+            <LatestInvoices user={user} />
+          </Suspense>
+        </div>
+      </main>
+    );
+  } else {
+    return <div>No user is logged in</div>;
   }
-  return (
-    <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard <span className="text-sm ml-4">{user.name}</span>
-      </h1>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardsSkeleton />}>
-          <CardWrapper />
-        </Suspense>
-      </div>
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart />
-        </Suspense>
-        <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices user={user} />
-        </Suspense>
-      </div>
-    </main>
-  );
 }
--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -20,25 +20,33 @@ export default async function Page({
     page?: string;
   };
 }) {
-  const { user } = await auth();
   const query = searchParams?.query || "";
-  const currentPage = Number(searchParams?.page) || 1;
-  const totalPages = await fetchCostumersPages(user, query);
-  return (
-    <div className="w-full">
-      <div className="flex w-full items-center justify-between">
-        <h1 className={`${lusitana.className} text-2xl`}>Alumnes</h1>
+  const { user } = await auth();
+  if (user) {
+    const totalPages = await fetchCostumersPages(user, query);
+
+    const currentPage = Number(searchParams?.page) || 1;
+    return (
+      <div className="w-full">
+        <div className="flex w-full items-center justify-between">
+          <h1 className={`${lusitana.className} text-2xl`}>Alumnes</h1>
+        </div>
+        <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+          <Search placeholder="Buscar Alumne..." />
+          <CreateInvoice />
+        </div>
+        <Suspense
+          key={query + currentPage}
+          fallback={<CustomerTableSkeleton />}
+        >
+          <CustomersTable query={query} currentPage={currentPage} />
+        </Suspense>
+        <div className="mt-5 flex w-full justify-center">
+          <Pagination totalPages={totalPages} />
+        </div>
       </div>
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Buscar Alumne..." />
-        <CreateInvoice />
-      </div>
-      <Suspense key={query + currentPage} fallback={<CustomerTableSkeleton />}>
-        <CustomersTable query={query} currentPage={currentPage} />
-      </Suspense>
-      <div className="mt-5 flex w-full justify-center">
-        <Pagination totalPages={totalPages} />
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return <div>No user is logged in</div>;
+  }
 }
