2e148284b4f397d53d3ef740a915d21300fdea46
Albert Céspedes Esteve <albertcespedes@gmail.com>
2023-12-21 11:16:38 +0100

    lasto

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -15,31 +15,26 @@ export default async function Page() {
   const { user } = await auth();
   const userData = user?.data || { displayName: "Anónima" };
 
-  if (user) {
-    // Use the `user` variable here
-    const reve = await fetchRevenuePerMonth(userData);
+  const reve = await fetchRevenuePerMonth(userData);
 
-    return (
-      <main>
-        <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-          Dashboard <span className="text-sm ml-4">{userData?.name}</span>
-        </h1>
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-          <Suspense fallback={<CardsSkeleton />}>
-            <CardWrapper />
-          </Suspense>
-        </div>
-        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-          <Suspense fallback={<RevenueChartSkeleton />}>
-            <RevenueChart />
-          </Suspense>
-          <Suspense fallback={<LatestInvoicesSkeleton />}>
-            <LatestInvoices user={userData} />
-          </Suspense>
-        </div>
-      </main>
-    );
-  } else {
-    return <div>No user is logged in</div>;
-  }
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard <span className="text-sm ml-4">{userData?.name}</span>
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices user={userData} />
+        </Suspense>
+      </div>
+    </main>
+  );
 }
